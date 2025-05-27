e09155f10fe200274c27d2661acdf77c3525f7e3
javier1511 <javiercastillo270987@gmail.com>
2025-05-27 17:58:58 -0600

    revisiondeseguridadrutas

--- backEnd/src/routes/dailyReportByDate.js
+++ backEnd/src/routes/dailyReportByDate.js
@@ -1,7 +1,9 @@
 import {Router} from  "express";
 const router = Router();
 import * as dailyReportByDate from "../controllers/report.sales.controller.js"
+import {authJwt} from "../middlewares"
 
-router.get("/", dailyReportByDate.getSalesReport);
+
+router.get("/", authJwt.verifyToken, dailyReportByDate.getSalesReport);
 
 export default router;
--- backEnd/src/routes/dailyUserReportByDate.js
+++ backEnd/src/routes/dailyUserReportByDate.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 const router = new Router();
 
 import * as dailyReportUser from "../controllers/user.salesReport.controller.js";
+import {authJwt} from "../middlewares"
 
-router.get("/", dailyReportUser.getUserReport);
+
+router.get("/", authJwt.verifyToken, dailyReportUser.getUserReport);
 
 export default router
